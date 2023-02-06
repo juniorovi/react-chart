9512029f0fd25f3926172b647dba77e696f29449
juniorovi <mdtarikulislam258000@gmail.com>
2023-02-06 08:58:11 +0600

    Spring Animation use in react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import MylineChart from './components/MyLinrChart/MylineChart';
 import SpecialChart from './components/SpecialChart/SpecialChart';
+import { useSpring, animated } from '@react-spring/web';
+import { useState } from 'react';
 
 function App() {
+  const [flip, setFlip]=useState(false)
+  const springs = useSpring({
+    /* from: { x: 0 },
+    to: { x: 100 }, */
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    onRest: ()=>setFlip(!flip)
+  })
   return (
     <div className="App">
+      <animated.div style={springs}>Hello springs</animated.div>
       <MylineChart></MylineChart>
       <SpecialChart></SpecialChart>
     </div>
